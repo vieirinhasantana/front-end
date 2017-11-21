a453dc1a4832dc0f8a163150c511717dc917e0ee
Daniel Vieira <daniel.vieira@obers.com.br>
2017-11-20 23:59:17 -0200

    Commit initial

---
+++ src/components/Dashboard.vue
@@ -0,0 +1,16 @@
+<template>
+    <div id="app-wrapper">
+      <sidebar></sidebar>
+      <navbar></navbar>
+    </div>
+</template>
+<script>
+import SideBar from './SideBar'
+import NavBar from './NavBar'
+export default {
+  components: {
+    sidebar: SideBar,
+    navbar: NavBar
+  }
+}
+</script>
---
+++ src/components/SideBar.vue
@@ -0,0 +1,27 @@
+<template>
+    <aside class="main-sidebar main-menu-html" data-menu-on-load="primary">
+        <div class="nav-container">
+            <nav>
+                <div class="menu-scroll">
+                    <ul class="main-menu--brand main-menu">
+                        <li class="main-menu__brand has-submenu" data-index="0" data-name="brand">
+                            <a>
+                                <div class="main-sidebar__logo"></div>
+                            </a>
+                            <div class="submenu submenu--system js-flex-start">
+                                <a class="submenu__mobile-return">
+                                    <i class="zmdi zmdi-arrow-left"></i> <span>Brand Menu</span>
+                                </a>
+                            </div>
+                        </li>
+                    </ul>
+                    <ul class="main-menu main-menu--primary">
+                        <li data-index="0" data-name="dashboard">
+                            <a href="index.html"><i class="zmdi zmdi-view-dashboard"></i> <span class="text">Dashboard</span></a>
+                        </li>
+                    </ul>
+                </div>
+            </nav>
+        </div>
+    </aside>
+</template>

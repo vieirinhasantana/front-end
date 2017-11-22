ef953c34225ccf6ba55debf96b73633d951774d9
Daniel Vieira <daniel.vieira@obers-server.intra>
2017-11-22 17:28:53 -0200

    ALTERAÇÃO DE AUTENTICAÇÃO

--- src/utils/api.js
+++ src/utils/api.js
@@ -2,5 +2,5 @@ import axios from 'axios'
 
 const BASE_URL = location.hostname === 'localhost' ? 'http://localhost:7020/' : 'http://35.197.122.215/'
 export function sign (email, password) {
-  return axios.get(BASE_URL, 'user/sign')
+  return axios.post(BASE_URL + 'user/sign')
 }

9ffaf231ea9cc05d3dc808a879dc529498be27d1
Daniel Vieira <daniel.vieira@obers-server.intra>
2017-11-23 09:41:22 -0200

    Mudança de endereco

--- src/utils/api.js
+++ src/utils/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios'
 
-const BASE_URL = location.hostname === 'localhost' ? 'http://api:7020/' : 'http://api:7020/'
+const BASE_URL = location.hostname === 'localhost' ? 'http://localhost:7020/' : 'http://api:7020/'
 export function sign (email, password) {
   axios.defaults.headers.common['Authorization'] = 'Basic ' + btoa(email + ':' + password)
   return axios.post(BASE_URL + 'user/sign')

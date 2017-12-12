8c74670ad241daaf948f1174854384cfd4fd7df8
Daniel Vieira <daniel.vieira@obers-server.intra>
2017-12-12 11:17:17 -0200

    changes

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -4,17 +4,24 @@
       <v-toolbar-title class="white--text">
         <img src="/static/img/logo.png" alt="" />
       </v-toolbar-title>
+      <v-flex md2 align-justfy offset-md1>
+        <v-form>
+          <v-text-field light solo prepend-icon="search" placeholder="Search..." class="unibank-search-input"></v-text-field>
+        </v-form>
+      </v-flex>
       <v-spacer></v-spacer>
-      <v-btn class="button button_primary button_money_transfer">
-        MONEY TRANSFER
-      </v-btn>
-      <v-badge overlap >
-        <span slot="badge">3</span>
-        <v-btn icon class="button_navbar_unibank">
-          <v-icon>chat_bubble_outline</v-icon>
-        </v-btn>
-      </v-badge>
-      <v-btn flat class="button_navbar_unibank">Logout <v-icon right>power_settings_new</v-icon></v-btn>
+      <v-flex md2>
+        <v-btn flat class="button button_primary button_money_transfer">MONEY TRANSFER</v-btn>  
+      </v-flex>
+      <v-flex md1>
+        <v-btn flat class="button_navbar_unibank" @click="logout">Logout <v-icon right>power_settings_new</v-icon></v-btn>
+      </v-flex>
+       <v-flex md1>
+        <v-btn flat class="button_navbar_unibank" >Logout <v-icon right>power_settings_new</v-icon></v-btn>
+      </v-flex>
+      <v-flex md1>
+        <v-btn flat class="button_navbar_unibank" @click="logout">Logout <v-icon right>power_settings_new</v-icon></v-btn>
+      </v-flex>
     </v-toolbar>
   </div>
 </template>
